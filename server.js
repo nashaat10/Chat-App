04fdcd503f2ba062f2e0dc4b675eb9958eb71b2e
nashaat10 <94199891+nashaat10@users.noreply.github.com>
2024-09-04 15:14:36 +0300

    up

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const http = require("http");
 const socketIo = require("socket.io");
+const formatMessage = require("./utils/messages");
 
 const app = express();
 const server = http.createServer(app);
@@ -9,21 +10,25 @@ const io = socketIo(server);
 
 app.use(express.static(path.join(__dirname, "public")));
 
+const botName = "ChatSpot Bot";
 io.on("connection", (socket) => {
   // used for single client
   //welcome current user
-  socket.emit("message", "Welcome to chatSpot");
+  socket.emit("message", formatMessage(botName, "Welcome to chatSpot"));
 
   // used to broadcast to all clients except the one who is connected when he is connected
-  socket.broadcast.emit("message", "A user has joined the chat");
+  socket.broadcast.emit(
+    "message",
+    formatMessage(botName, "A user has joined the chat")
+  );
 
   socket.on("disconnect", () => {
     // io.emit used to all clients
-    io.emit("message", "A user has left the chat");
+    io.emit("message", formatMessage(botName, "A user has left the chat"));
   });
   socket.on("chatMessage", (msg) => {
     console.log(msg);
-    io.emit("message", msg);
+    io.emit("message", formatMessage("USER", msg));
   });
 });
 
