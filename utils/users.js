5556fea3a0319a0482b946580feae687a91f9233
nashaat10 <94199891+nashaat10@users.noreply.github.com>
2024-09-13 18:50:26 +0300

    add fun

--- utils/users.js
+++ utils/users.js
@@ -11,7 +11,20 @@ function getCurrentUser(id) {
   return users.find((user) => user.id === id);
 }
 
+function userLeave(id) {
+  const index = users.findIndex((user) => user.id === id);
+  if (index != -1) {
+    return users.splice(index, 1);
+  }
+}
+
+function getRoomUsers(room) {
+  return users.filter((user) => user.room === room);
+}
+
 module.exports = {
   userJoin,
   getCurrentUser,
+  userLeave,
+  getRoomUsers,
 };
